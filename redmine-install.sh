1cc91d51054ce42e9bca340ed17408d64ea74ce4
antonstrand <jag@antonstrand.se>
2019-11-04 13:08:58 +0100

    Remove all need for user input

--- redmine-install.sh
+++ redmine-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
   
 # Script for setting up Redmine 4.0.2 on Ubuntu 18.04.
 # Following this blog post: https://www.rosehosting.com/blog/how-to-install-redmine-on-ubuntu-18-04/
@@ -7,54 +7,63 @@
 
 # Generate password for database
 PASSWORD=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+ROOT_PASSWORD=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+DOMAIN_NAME=$(sed 's/.\{1\}$//' <<< $(dig +short -x $IP))
+
+echo "Setting up Redmine"
+echo "IP: 		$IP"
+echo "Domain name: 	$DOMAIN_NAME"
 
 echo ##################################
 echo Step 1: Update
 echo ##################################
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 echo ##################################
 echo Step 2: Install MySQL
 echo ##################################
-sudo apt-get install mysql-server
+sudo apt-get -y install mysql-server
 
 echo Enable auto restart after reboot
-sudo systemctl enable mysql
+sudo systemctl enable mysql -y
 
 echo Increase security
-echo "Next up is to set up important security tasks like a root password, disable remote root login, remove anonymous users, etc. If the script asks for the root password, just press the [Enter] key, as no root password is set by default."
-sudo mysql_secure_installation
+sudo mysql -u root << EOF
+UPDATE mysql.user SET Password=PASSWORD('$ROOT_PASSWORD') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
+FLUSH PRIVILEGES;
+EOF
 
 echo ##################################
 echo Step 3: Create DB for Redmine
 echo ##################################
-echo "Use the password you used during the security section."
-echo "Note: password will be hidden when typing"
-read -s passwd
-sudo mysql -uroot -p${passwd} -e "CREATE DATABASE IF NOT EXISTS redmine_db;GRANT ALL PRIVILEGES ON redmine_db.* TO 'redmine_user'@'localhost' IDENTIFIED BY '$PASSWORD';FLUSH PRIVILEGES;"
+sudo mysql -uroot -p${ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS redmine_db;GRANT ALL PRIVILEGES ON redmine_db.* TO 'redmine_user'@'localhost' IDENTIFIED BY '$PASSWORD';FLUSH PRIVILEGES;"
 
 echo ##################################
 echo Step 4: Install Ruby
 echo ##################################
-sudo apt-get install ruby-full
+sudo apt-get -y install ruby-full
 
 echo ##################################
 echo Step 5: Install Nginx and Passenger
 echo ##################################
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo apt-get install dirmngr gnupg apt-transport-https ca-certificates
+sudo apt-get -y install dirmngr gnupg apt-transport-https ca-certificates
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 561F9B9CAC40B2F7
 sudo add-apt-repository 'deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main'
-sudo apt-get update
-sudo apt-get install libnginx-mod-http-passenger
+sudo apt-get -y update
+sudo apt-get -y install libnginx-mod-http-passenger
 
 echo ##################################
 echo Step 6: Download and Install Redmine
 echo ##################################
-sudo apt-get install build-essential libmysqlclient-dev imagemagick libmagickwand-dev
+sudo apt-get -y install build-essential libmysqlclient-dev imagemagick libmagickwand-dev
 sudo wget https://www.redmine.org/releases/redmine-4.0.2.zip -O /opt/redmine.zip
 cd /opt
 sudo unzip redmine.zip
@@ -86,67 +95,65 @@ echo ##################################
 echo  Step 8: Let\'s encrypt
 echo ##################################
 echo  1. Add Certbot PPA
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get update
+sudo apt-get -y update
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get -y update
 
 echo  2. Install Certbot
-sudo apt-get install certbot python-certbot-nginx
+sudo apt-get -y install certbot python-certbot-nginx
 
 echo  3. Setup NGNIX
-sudo certbot --nginx
-
-read -p "Please enter your domain name: " domainName
+sudo certbot --non-interactive --agree-tos -d $DOMAIN_NAME -m $1 --nginx
 
 echo "# Redirect HTTP -> HTTPS
 server {
     listen 80;
-    server_name www.$domainName $domainName;
+    server_name www.$DOMAIN_NAME $DOMAIN_NAME;
 
-    return 301 https://$domainName$request_uri;
+    return 301 https://$DOMAIN_NAME$request_uri;
 }
 
 # Redirect WWW -> NON WWW
 server {
     listen 443 ssl http2;
-    server_name www.$domainName;
+    server_name www.$DOMAIN_NAME;
 
-    ssl_certificate /etc/letsencrypt/live/$domainName/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domainName/privkey.pem;
-    ssl_trusted_certificate /etc/letsencrypt/live/$domainName/chain.pem;
+    ssl_certificate /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem;
+    ssl_trusted_certificate /etc/letsencrypt/live/$DOMAIN_NAME/chain.pem;
 
-    return 301 https://$domainName$request_uri;
+    return 301 https://$DOMAIN_NAME$request_uri;
 }
 
 server {
     listen 443 ssl http2;
-    server_name $domainName;
+    server_name $DOMAIN_NAME;
 
     root /opt/redmine/public;
 
     # SSL parameters
-    ssl_certificate /etc/letsencrypt/live/$domainName/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$domainName/privkey.pem;
-    ssl_trusted_certificate /etc/letsencrypt/live/$domainName/chain.pem;
+    ssl_certificate /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem;
+    ssl_trusted_certificate /etc/letsencrypt/live/$DOMAIN_NAME/chain.pem;
 
     # log files
-    access_log /var/log/nginx/$domainName.access.log;
-    error_log /var/log/nginx/$domainName.error.log;
+    access_log /var/log/nginx/$DOMAIN_NAME.access.log;
+    error_log /var/log/nginx/$DOMAIN_NAME.error.log;
 
     passenger_enabled on;
     passenger_min_instances 1;
     client_max_body_size 10m;
-}" | sudo tee -a /etc/nginx/sites-available/$domainName.conf > /dev/null
+}" | sudo tee -a /etc/nginx/sites-available/$DOMAIN_NAME.conf > /dev/null
 
-sudo ln -s /etc/nginx/sites-available/$domainName.conf /etc/nginx/sites-enabled/$domainName.conf
+sudo ln -s /etc/nginx/sites-available/$DOMAIN_NAME.conf /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
 sudo nginx -t
 sudo service nginx reload
 
 echo ##################################
 echo  Finally: Clean
 echo ##################################
-sudo apt-get autoremove
+sudo apt-get -y autoremove
 
-echo "Redmine is installed and can be found at www.$domainName"
+echo "Redmine is installed and can be found at www.$DOMAIN_NAME"
